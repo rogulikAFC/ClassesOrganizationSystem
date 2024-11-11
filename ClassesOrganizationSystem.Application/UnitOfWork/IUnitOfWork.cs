1d57b7c687e8861b3cc1c017b87bad62df0f79b1
rogulik <malvla2008@gmail.com>
2024-11-11 07:54:56 +0300

    Unit of work repository written

---
+++ ClassesOrganizationSystem.Application/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using ClassesOrganizationSystem.Application.UnitOfWork.Repositories;
+
+namespace ClassesOrganizationSystem.Application.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        ISchoolRepository SchoolRepository { get; }
+
+        IScheduleRepository ScheduleRepository { get; }
+
+        IRoomRepository RoomRepository { get; }
+
+        IUserRepository UserRepository { get; }
+
+        Task SaveChangesAsync();
+    }
+}
